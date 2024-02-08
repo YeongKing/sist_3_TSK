d366de7d8f6c424e26d919ae5ac81cef7a5b44b9
ljhee92 <weilainib7@gmail.com>
2024-02-08 14:27:35 +0900

    이주희 추가

--- src/tsk_0213/Test.java
+++ src/tsk_0213/Test.java
@@ -8,6 +8,9 @@ public class Test {
 		System.out.println("test");
 
 		System.out.println("만약에 fetch를 하고싶다면?");
+		System.out.println("윤웅찬");
+		System.out.println("김무영");
+		System.out.println("이주희");
 
 	}//main
 }
